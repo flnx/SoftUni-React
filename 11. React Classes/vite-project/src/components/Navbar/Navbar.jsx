ac42cc577ae89b0cc9df11e3dd5698dd94a7a9ab
flnx <32278279+flnx@users.noreply.github.com>
2023-02-28 02:58:21 +0200

    Refactoring Navbar with a class Component

--- 11. React Classes/vite-project/src/components/Navbar/Navbar.jsx
+++ 11. React Classes/vite-project/src/components/Navbar/Navbar.jsx
@@ -1,33 +1,36 @@
 import { AuthContext } from '../../context/AuthContext';
 import { Link } from 'react-router-dom';
-import { useContext } from 'react';
+import { Component } from 'react';
 
-export const Navbar = () => {
-    const { auth } = useContext(AuthContext);
+export class Navbar extends Component {
+    static contentType = AuthContext;
 
-    return (
-        <nav>
-            <div className="nav-wrapper">
-                <h1>
-                    <Link className="home" to="/">
-                        GamesPlay
-                    </Link>
-                </h1>
-                <div className="navbar">
-                    <Link to="/catalog">All games</Link>
-                    {auth && auth.accessToken ? (
-                        <div id="user">
-                            <Link to="/create">Create Game</Link>
-                            <Link to="/logout">Logout</Link>
-                        </div>
-                    ) : (
-                        <div id="guest">
-                            <Link to="/login">Login</Link>
-                            <Link to="/register">Register</Link>
-                        </div>
-                    )}
+    render() {
+        const { auth } = this.context;
+        return (
+            <nav>
+                <div className="nav-wrapper">
+                    <h1>
+                        <Link className="home" to="/">
+                            GamesPlay
+                        </Link>
+                    </h1>
+                    <div className="navbar">
+                        <Link to="/catalog">All games</Link>
+                        {auth && auth.accessToken ? (
+                            <div id="user">
+                                <Link to="/create">Create Game</Link>
+                                <Link to="/logout">Logout</Link>
+                            </div>
+                        ) : (
+                            <div id="guest">
+                                <Link to="/login">Login</Link>
+                                <Link to="/register">Register</Link>
+                            </div>
+                        )}
+                    </div>
                 </div>
-            </div>
-        </nav>
-    );
-};
+            </nav>
+        );
+    }
+}
--- 11. React Classes/vite-project/src/pages/Catalog/Catalog.jsx
+++ 11. React Classes/vite-project/src/pages/Catalog/Catalog.jsx
@@ -5,10 +5,6 @@ import { AllGames } from './AllGames';
 import { Error } from '../../components/Error';
 
 export class Catalog extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     static contextType = GamesContext;
 
     render() {
